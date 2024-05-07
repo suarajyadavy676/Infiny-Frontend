3012e748b1385cc4f2c45933c59939a422a38bf7
Suraj Yadav <surajyadavy676@gmail.com>
2024-05-08 00:36:58 +0530

    add content in login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,84 +1,101 @@
 import axios from "axios";
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import { useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function Login() {
   const toast = useToast();
-  let navigate = useNavigate()
-  console.log(`${import.meta.env.REACT_APP_API_URL}user/signIn`)
-  let [loginData,setLoginData] = useState({password:"",email:""})
-  const handleClick = async(e)=>{
-    e.preventDefault()
-    console.log(loginData)
+  let navigate = useNavigate();
+  console.log(`${import.meta.env.REACT_APP_API_URL}user/signIn`);
+  let [loginData, setLoginData] = useState({ password: "", email: "" });
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log(loginData);
     try {
       //for development
       // let res = await axios.post("http://localhost:3000/user/signIn",loginData)
       // for production
-      let res = await axios.post(`${import.meta.env.REACT_APP_API_URL}user/signIn`,loginData)
-      console.log(res)
-      localStorage.setItem('token',res.data.token)
-      setLoginData({password:"",email:""})
+      let res = await axios.post(
+        `${import.meta.env.REACT_APP_API_URL}user/signIn`,
+        loginData
+      );
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
+      setLoginData({ password: "", email: "" });
       toast({
         title: "Login Success",
         description: `Hello, ${res.data.name}! Good to see you again.`,
         status: "success",
         duration: 9000,
         isClosable: true,
-      })
-      navigate('/dashboard')
+      });
+      navigate("/dashboard");
     } catch (error) {
-      console.log("error in login time")
+      console.log("error in login time");
     }
-  }
+  };
   return (
-    <div className="">
-      <div className="max-w-xl py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl mx-auto">
-        <form method="POST" onSubmit={handleClick}>
-          <div className="mb-6">
-            <label htmlFor="name" className="block text-gray-800 font-bold">
-              Email:
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={loginData.email}
-              autoComplete="username"
-              required
-              onChange={(e)=>setLoginData({...loginData,email:e.target.value})}
-              placeholder="@email"
-              className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="block text-gray-800 font-bold">
-              Password:
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={loginData.password}
-              autoComplete="current-password"
-              required
-              placeholder="@password"
-              onChange={(e)=>setLoginData({...loginData,password:e.target.value})}
-              className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
-            />
-            <a
-              href="#"
-              className="text-sm font-thin text-gray-800 hover:underline mt-2 inline-block hover:text-indigo-600"
+    <>
+      <Helmet>
+        <title>Login</title>
+        <meta name="description" content="Secure and seamless login to access personalized features, settings, and content tailored to enhance your user experience effectively" />
+      </Helmet>
+      <div className="">
+        <div className="max-w-xl py-6 px-8 h-80 mt-20 bg-white rounded shadow-xl mx-auto">
+          <form method="POST" onSubmit={handleClick}>
+            <div className="mb-6">
+              <label htmlFor="name" className="block text-gray-800 font-bold">
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={loginData.email}
+                autoComplete="username"
+                required
+                onChange={(e) =>
+                  setLoginData({ ...loginData, email: e.target.value })
+                }
+                placeholder="@email"
+                className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-gray-800 font-bold">
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={loginData.password}
+                autoComplete="current-password"
+                required
+                placeholder="@password"
+                onChange={(e) =>
+                  setLoginData({ ...loginData, password: e.target.value })
+                }
+                className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600"
+              />
+              <a
+                href="#"
+                className="text-sm font-thin text-gray-800 hover:underline mt-2 inline-block hover:text-indigo-600"
+              >
+                Forget Password
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded"
             >
-              Forget Password
-            </a>
-          </div>
-          <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">
-            Login
-          </button>
-        </form>
+              Login
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -2,9 +2,10 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "@chakra-ui/react";
+import { Helmet } from "react-helmet";
 
 function SignUp() {
-  console.log(`${import.meta.env.REACT_APP_API_URL}user/register`)
+  console.log(`${import.meta.env.REACT_APP_API_URL}user/register`);
   const toast = useToast();
   let navigate = useNavigate();
   let [userData, setUserData] = useState({
@@ -24,7 +25,10 @@ function SignUp() {
       // );
 
       // for production
-      let res = await axios.post(`${import.meta.env.REACT_APP_API_URL}user/register`,userData);
+      let res = await axios.post(
+        `${import.meta.env.REACT_APP_API_URL}user/register`,
+        userData
+      );
       console.log(res);
       if (res.data == "user already present") {
         return setAlResData(res.data);
@@ -35,7 +39,7 @@ function SignUp() {
         status: "success",
         duration: 9000,
         isClosable: true,
-      })
+      });
       return navigate("/login");
     } catch (error) {
       console.log("error");
@@ -51,123 +55,131 @@ function SignUp() {
   // }
   // getData()
   return (
-    <div>
-      <div className=" flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div className="bg-white shadow-md rounded-md p-6">
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://www.svgrepo.com/show/499664/user-happy.svg"
-              alt=""
-            />
+    <>
+      <Helmet>
+        <title>SignUp</title>
+        <meta
+          name="description"
+          content="Sign up now for exclusive benefits! Fast, secure registration process for access to premium content and personalized offers"
+        />
+      </Helmet>
+      <div>
+        <div className=" flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+          <div className="w-full max-w-md space-y-8">
+            <div className="bg-white shadow-md rounded-md p-6">
+              <img
+                className="mx-auto h-12 w-auto"
+                src="https://www.svgrepo.com/show/499664/user-happy.svg"
+                alt=""
+              />
 
-            <h2 className="my-3 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign up for an account
-            </h2>
+              <h2 className="my-3 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Sign up for an account
+              </h2>
 
-            <form className="space-y-6" method="POST" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="new-password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Username
-                </label>
-                <div className="mt-1">
-                  <input
-                    name="username"
-                    type="username"
-                    value={userData.username}
-                    onChange={(e) =>
-                      setUserData({ ...userData, username: e.target.value })
-                    }
-                    required
-                    className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                  />
+              <form className="space-y-6" method="POST" onSubmit={handleSubmit}>
+                <div>
+                  <label
+                    htmlFor="new-password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="username"
+                      type="username"
+                      value={userData.username}
+                      onChange={(e) =>
+                        setUserData({ ...userData, username: e.target.value })
+                      }
+                      required
+                      className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <div className="mt-1">
-                  <input
-                    name="email"
-                    type="email-address"
-                    autoComplete="email-address"
-                    value={userData.email}
-                    onChange={(e) =>
-                      setUserData({ ...userData, email: e.target.value })
-                    }
-                    required
-                    className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                  />
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="email"
+                      type="email-address"
+                      autoComplete="email-address"
+                      value={userData.email}
+                      onChange={(e) =>
+                        setUserData({ ...userData, email: e.target.value })
+                      }
+                      required
+                      className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    name="password"
-                    type="password"
-                    autoComplete="password"
-                    value={userData.password}
-                    onChange={(e) =>
-                      setUserData({ ...userData, password: e.target.value })
-                    }
-                    required
-                    className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                  />
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="password"
+                      type="password"
+                      autoComplete="password"
+                      value={userData.password}
+                      onChange={(e) =>
+                        setUserData({ ...userData, password: e.target.value })
+                      }
+                      required
+                      className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Confirm Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    name="confirm_password"
-                    type="password"
-                    autoComplete="confirm-password"
-                    required
-                    className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                  />
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Confirm Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      name="confirm_password"
+                      type="password"
+                      autoComplete="confirm-password"
+                      required
+                      className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div>
-                <button
-                  type="submit"
-                  
-                  className="flex w-full justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
-                >
-                  Register Account
-                </button>
-                {alResData == "" ? null : (
-                  <h1 className="text-center text-red-600 text-lg">
-                    {alResData}
-                  </h1>
-                )}
-              </div>
-            </form>
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+                  >
+                    Register Account
+                  </button>
+                  {alResData == "" ? null : (
+                    <h1 className="text-center text-red-600 text-lg">
+                      {alResData}
+                    </h1>
+                  )}
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
