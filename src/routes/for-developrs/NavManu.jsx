bb28b423eee9b9e2f0a61ce504dfc455e4dbc576
Suraj Yadav <surajyadavy676@gmail.com>
2024-08-16 22:45:35 +0530

    add logout functionality

--- src/routes/for-developrs/NavManu.jsx
+++ src/routes/for-developrs/NavManu.jsx
@@ -1,14 +1,23 @@
-import React from "react";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-} from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { TbUser } from "react-icons/tb";
 import { GoChevronDown } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../components/AuthContextProvider";
 
 function NavManu() {
+  let navigate = useNavigate();
+  const { setToken } = useContext(AuthContext);
+  function handleClick() {
+    // Clear local storage
+    localStorage.clear();
+
+    // Clear authentication context state
+    setToken(null); // or use a function to clear the context state
+
+    // Navigate to the home page or login page
+    navigate("/");
+  }
   return (
     <div>
       <Menu>
@@ -18,9 +27,11 @@ function NavManu() {
         </MenuButton>
         <MenuList>
           <div className="text-black">
-          <div className="text-center my-4">
-            <span className="bg-blue-500 text-white py-2 px-8">Hackos: 270</span>
-          </div>
+            <div className="text-center my-4">
+              <span className="bg-blue-500 text-white py-2 px-8">
+                Hackos: 270
+              </span>
+            </div>
             <MenuItem> Profile</MenuItem>
             <MenuItem>Dark Mode</MenuItem>
             <MenuItem>Leaderboard</MenuItem>
@@ -29,7 +40,7 @@ function NavManu() {
             <MenuItem>Network</MenuItem>
             <MenuItem>Submissions</MenuItem>
             <MenuItem>Administration</MenuItem>
-            <MenuItem>Logout</MenuItem>
+            <MenuItem onClick={handleClick}>Logout</MenuItem>
           </div>
         </MenuList>
       </Menu>
